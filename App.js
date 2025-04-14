60c7d79a88d8bd530b20a558545eeb32893ddbc8
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-14 01:50:37 -0300

    feat(style): adds initial component structure and property names for styling

--- App.js
+++ App.js
@@ -3,12 +3,30 @@ import {
   View,
   Text,
   StyleSheet,
+  Image,
+  TouchableOpacity
 } from 'react-native';
 
 class App extends Component{
   render(){
     return(
       <View style={styles.container}>
+        <Image 
+        source={'./src/images/stopwatch.png'}
+        style={styles.stopwatch} 
+        />
+
+        <Text style={styles.timer}>0.0</Text>
+
+        <View style={styles.btnAre}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Go!</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Clean</Text>
+          </TouchableOpacity>
+        </View>
 
       </View>
     );
