4725c398f1d71d51e2372db953bdbf1556620b9b
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-14 02:33:53 -0300

    refactor(timer): improves the readability of the interface
    with the management of the state of the buttons

--- App.js
+++ App.js
@@ -1,90 +1,96 @@
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
-class App extends Component{
-
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      number: 0
+      number: 0,
+      buttonName: 'Go!',
     };
+
+    this.timer = null;
 
     this.go = this.go.bind(this);
     this.clean = this.clean.bind(this);
   }
 
-  go(){
-    setInterval(() => {
-      this.setState({
-        number: this.state.number + 0.1
-      });
-    }, 100);
+  go() {
+    if (this.timer != null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      
+      this.setState({buttonName: 'Go!'});
+    } else {
+
+      this.timer = setInterval(() => {
+        this.setState({
+          number: this.state.number + 0.1,
+        });
+      }, 100);
+      
+      this.setState({buttonName: 'Stop'});
+    }
   }
 
-  clean(){
+  clean() {}
 
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.container}>
-        <Image source={require('./src/images/stopwatch.png')} style={styles.stopwatch} />
+        <Image
+          source={require('./src/images/stopwatch.png')}
+          style={styles.stopwatch}
+        />
 
         <Text style={styles.timer}>{this.state.number.toFixed(1)}</Text>
 
         <View style={styles.btnArea}>
           <TouchableOpacity style={styles.btn} onPress={this.go}>
-            <Text style={styles.btnText}>Go!</Text>
+            <Text style={styles.btnText}>{this.state.buttonName}</Text>
           </TouchableOpacity>
 
           <TouchableOpacity style={styles.btn} onPress={this.clean}>
             <Text style={styles.btnText}>Clean</Text>
           </TouchableOpacity>
         </View>
-
       </View>
     );
-  };
-};
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#00aeef',
   },
-  timer:{
+  timer: {
     marginTop: -160,
     color: '#fff',
     fontSize: 65,
     fontWeight: 'bold',
-  },  
-  btnArea:{
+  },
+  btnArea: {
     flexDirection: 'row',
     marginTop: 70,
-    height: 40
+    height: 40,
   },
   btn: {
     flex: 1,
-    alignItems:'center',
+    alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fff',
     height: 40,
     margin: 17,
     borderRadius: 9,
-    },
-    btnText: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: '#00aeef'
-    }
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00aeef',
+  },
 });
 
 export default App;
